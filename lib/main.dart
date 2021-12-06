e31d8ae9ddc62b95a713d0f7202230d0e8570c4f
Yasser Benmman <yasserbenmman@gmail.com>
2021-12-06 14:49:12 +0100

    budget tracker with flutter and notion

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:budget_tracker/budget_repository.dart';
+import 'package:budget_tracker/failure_model.dart';
+import 'package:budget_tracker/item_model.dart';
+import 'package:budget_tracker/spending_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notion Budget Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: BudgetScreen(),
+    );
+  }
+}
+
+class BudgetScreen extends StatefulWidget {
+  BudgetScreen() : super();
+
+  @override
+  State<BudgetScreen> createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  late Future<List<Item>> _futureItems;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _futureItems = BudgetRepository().getItems();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Budget Tracker"),
+        centerTitle: true,
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _futureItems = BudgetRepository().getItems();
+          setState(() {});
+        },
+        child: FutureBuilder<List<Item>>(
+          future: _futureItems,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final items = snapshot.data!;
+              return ListView.builder(
+                itemCount: items.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) return SpendingChart(items: items);
+                  final item = items[index - 1];
+                  return Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 2,
+                          color: getCategoryColor(item.category),
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                            blurRadius: 6,
+                          )
+                        ]),
+                    child: ListTile(
+                      title: Text(item.name),
+                      subtitle: Text("${item.category} - ${item.date}"),
+                      trailing: Text("-\$${item.price.toStringAsFixed(2)}"),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              final failure = snapshot.error as Failure;
+              return Center(
+                child: Text(failure.message),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Color getCategoryColor(String category) {
+  switch (category) {
+    case 'Entertaiment':
+      return Colors.red[400]!;
+    case 'Food':
+      return Colors.green[400]!;
+    case 'Personal':
+      return Colors.blue[400]!;
+    case 'Transportation':
+      return Colors.purple[400]!;
+    default:
+      return Colors.orange[400]!;
+  }
+}
